edfd537364077a493cde7fcff46ab53adcfc008c
Elmer Coronel <infcle@gmail.com>
2026-02-18 15:32:55 -0400

    Fix pratice
    Signed-off-by: Elmer Coronel <infcle@gmail.com>

--- src/test/java/proyecto/testng/DemoQaPracticeFormTest.java
+++ src/test/java/proyecto/testng/DemoQaPracticeFormTest.java
@@ -4,7 +4,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,10 +17,12 @@ import org.testng.annotations.Test;
 
 public class DemoQaPracticeFormTest {
 
-    public static String URL = "https://demoqa.com/automation-practice-form";
+    public static String BASEURL = "https://demoqa.com";
 
     WebDriver driver;
     WebDriverWait wait;
+    By formsCard = By.cssSelector("a[href='/forms']");
+    By practiceForm = By.cssSelector("a[href='/automation-practice-form']");
 
     By firstNameInput = By.id("firstName");
     By lastNameInput = By.id("lastName");
@@ -46,57 +47,68 @@ public class DemoQaPracticeFormTest {
         driver.manage()
             .window()
             .maximize();
-        driver.get(URL);
+        driver.get(BASEURL);
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
 
+
     @Test
     public void submitPracticeFormTest() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(firstNameInput))
-            .sendKeys("Elmer");
-        driver.findElement(lastNameInput)
-            .sendKeys("Coronel");
-        driver.findElement(emailInput)
-            .sendKeys("elmer@test.com");
-        driver.findElement(genderMaleLabel)
-            .click();
-        driver.findElement(mobileInput)
-            .sendKeys("7777777777");
+
+        WebElement card = wait.until(ExpectedConditions.elementToBeClickable(formsCard));
+        card.click();
+
+        WebElement formPractice = wait.until(ExpectedConditions.elementToBeClickable(practiceForm));
+        formPractice.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(firstNameInput));
+        WebElement inputName = driver.findElement(firstNameInput);
+        inputName.sendKeys("Elmer");
+
+        WebElement inputLastName = driver.findElement(lastNameInput);
+        inputLastName.sendKeys("Coronel");
+
+        WebElement inputEmail = driver.findElement(emailInput);
+        inputEmail.sendKeys("elmer@test.com");
+
+        WebElement labelGenderMale = driver.findElement(genderMaleLabel);
+        wait.until(ExpectedConditions.visibilityOf(labelGenderMale));
+        labelGenderMale.click();
+
+        WebElement inputMobile = driver.findElement(mobileInput);
+        inputMobile.sendKeys("7777777777");
 
         WebElement dateInput = driver.findElement(dateOfBirthInput);
         dateInput.sendKeys(Keys.CONTROL + "a");
         dateInput.sendKeys("15 Aug 1998");
         dateInput.sendKeys(Keys.ENTER);
 
-        driver.findElement(subjectsInput)
-            .sendKeys("Maths");
-        driver.findElement(subjectsInput)
-            .sendKeys(Keys.ENTER);
+        WebElement inputSubjets = driver.findElement(subjectsInput);
+        inputSubjets.sendKeys("Maths");
+        inputSubjets.sendKeys(Keys.ENTER);
 
-        driver.findElement(hobbySportsLabel)
-            .click();
+        WebElement labelHobbySports = driver.findElement(hobbySportsLabel);
+        labelHobbySports.click();
 
         Path filePath = Paths.get("src", "test", "resources", "upload-demo.txt")
             .toAbsolutePath();
-        driver.findElement(uploadPictureInput)
-            .sendKeys(filePath.toString());
 
-        driver.findElement(currentAddressInput)
-            .sendKeys("Av. Siempre Viva 123");
+        WebElement inputUploadPicture = driver.findElement(uploadPictureInput);
+        inputUploadPicture.sendKeys(filePath.toString());
 
-        driver.findElement(stateInput)
-            .sendKeys("NCR");
-        driver.findElement(stateInput)
-            .sendKeys(Keys.ENTER);
+        WebElement inputCurrentAddress = driver.findElement(currentAddressInput);
+        inputCurrentAddress.sendKeys("Av. Siempre Viva 123");
 
-        driver.findElement(cityInput)
-            .sendKeys("Delhi");
-        driver.findElement(cityInput)
-            .sendKeys(Keys.ENTER);
+        WebElement inputState = driver.findElement(stateInput);
+        inputState.sendKeys("NCR");
+        inputState.sendKeys(Keys.ENTER);
+
+        WebElement inputCity = driver.findElement(cityInput);
+        inputCity.sendKeys("Delhi");
+        inputCity.sendKeys(Keys.ENTER);
 
         WebElement submit = driver.findElement(submitButton);
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", submit);
-        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", submit);
+        submit.click();
 
         WebElement title = wait.until(ExpectedConditions.visibilityOfElementLocated(modalTitle));
         Assert.assertEquals(title.getText(), "Thanks for submitting the form");
